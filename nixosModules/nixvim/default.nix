2e39971950e1e1b2007e242c84b4f8155ac3c173
L0L1P0P1 <behradbadeli@gmail.com>
2025-03-01 21:23:04 +0330

    notify

--- nixosModules/nixvim/default.nix
+++ nixosModules/nixvim/default.nix
@@ -44,7 +44,6 @@
 				gitsigns.enable = true;
 				dressing.enable = true;
 				noice.enable = true;
-				notify.enable = true;
 				presence-nvim.enable = true;
 				render-markdown.enable = true;
 
@@ -176,6 +175,35 @@
 							"markdown"
 							"markdown_inline"
 						];
+					};
+				};
+
+				notify = {
+					enable = true;
+					settings = {
+						background_colour = "#000000";
+						fps = 30;
+						icons = {
+							debug = "";
+							error = "";
+							info = "";
+							trace = "✎";
+							warn = "";
+						};
+						level = "info";
+						max_height = 10;
+						max_width = 80;
+						minimum_width = 50;
+						on_close = {
+							__raw = "function() print('Window closed') end";
+						};
+						on_open = {
+							__raw = "function() print('Window opened') end";
+						};
+						render = "default";
+						stages = "fade_in_slide_out";
+						timeout = 5000;
+						top_down = true;
 					};
 				};
 			};
