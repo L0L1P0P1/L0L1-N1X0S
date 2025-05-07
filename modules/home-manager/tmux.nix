b5571072bbf88c01e48e60ac077d802d29d38d3c
Behrad Badeli <behradbadeli@gmail.com>
2025-05-07 12:22:03 +0330

    tmux fixes

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -30,20 +30,7 @@
 				yank
 				sensible
 				vim-tmux-navigator
-                {
-				    plugin = resurrect;
-                    # extraConfig = ''
-                    #     set -g @resurrect-strategy-nvim 'session'
-                    #     set -g @resurrect-capture-pane-contents 'on'
-                    # '';
-                }
-                {
-					plugin = continuum;
-					extraConfig = ''
-					   set -g @continuum-restore 'on'
-                       set -g @continuum-save-interval '5'
-					'';
-                }
+                gruvbox
 				{
 					plugin = tmux-sessionx;
 					extraConfig = ''
@@ -54,9 +41,20 @@
 						set -g @sessionx-additional-options "--color pointer:9,spinner:92,marker:46"
 					'';
 				}
-				{
-					plugin = gruvbox;
-				}
+                {
+				    plugin = resurrect;
+                    extraConfig = ''
+                        set -g @resurrect-strategy-nvim 'session'
+                        set -g @resurrect-capture-pane-contents 'on'
+                    '';
+                }
+                {
+					plugin = continuum;
+					extraConfig = ''
+					   set -g @continuum-restore 'on'
+                       set -g @continuum-save-interval '5'
+					'';
+                }
             ];
 		};
 	};
