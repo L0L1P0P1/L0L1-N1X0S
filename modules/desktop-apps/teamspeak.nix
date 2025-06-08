bd3449a30c55123beabbc9ec4aa5059ce7957978
L0L1P0P1 <behradbadeli@gmail.com>
2025-06-08 23:24:50 +0330

    teamspeak hosts

--- modules/desktop-apps/teamspeak.nix
+++ modules/desktop-apps/teamspeak.nix
@@ -11,6 +11,12 @@
   };
 
   config = lib.mkIf config.teamspeak.enable {
+    networking.extraHosts =
+      ''
+        127.0.0.1 blacklist.teamspeak.com
+        127.0.0.1 blacklist2.teamspeak.com
+      '';
+
     environment.systemPackages = [
       pkgs.teamspeak3
     ];
