976bce752380845b6c1a09b45bd49fc9bdd4026d
L0L1P0P1 <behradbadeli@gmail.com>
2026-08-26 12:48:56 +0330

    docker proxy

--- hosts/sitka/configuration.nix
+++ hosts/sitka/configuration.nix
@@ -267,6 +267,13 @@
     };
   };
 
+  # docker proxy
+  systemd.services.docker.environment = {
+    HTTP_PROXY = "http://127.0.0.1:10808";
+    HTTPS_PROXY = "http://127.0.0.1:10808";
+    NO_PROXY = "localhost,127.0.0.1,docker.internal";
+  };
+
   # Polkit
   security.polkit.enable = true;
   security.sudo = {
